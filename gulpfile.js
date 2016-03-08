67dc5cef7e551757be1bede13086dd9f6d6dbe0b
Tim Officer <timothyofficer@gmail.com>
2016-03-08 10:54:59 -0500

    use .env file for loading ENV vars, add mail chimp api key using env var

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const NODE_ENV = process.env.WERCKER_GIT_BRANCH || process.env.NODE_ENV || proce
 const ENV = setupEnv(NODE_ENV);
 const ENV_PROD = (ENV === 'production');
 
+const env = require('node-env-file');
+
+env(__dirname + '/.env');
+
 const appConfig = require('./config/appConfig')[ENV];
 
 const childProcess = require('child_process');
--- src/app/appConfig.js
+++ src/app/appConfig.js
@@ -4,5 +4,6 @@ angular.module('startupReviewApp').constant('appConfig', {
   DOMAIN: '@@DOMAIN',
   API_URL: '@@API',
   ENABLE_COOKIE_DOMAIN: '@@ENABLE_COOKIE_DOMAIN' === 'true' ? true : false,
-  ENABLE_SECURE_COOKIE: '@@ENABLE_SECURE_COOKIE' === 'true' ? true : false
+  ENABLE_SECURE_COOKIE: '@@ENABLE_SECURE_COOKIE' === 'true' ? true : false,
+  MAILCHIMP_API_KEY: '@@MAILCHIMP_API_KEY'
 });
