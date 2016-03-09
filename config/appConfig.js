2785681759d6411f6a4858aae21db27872331a5c
Tim Officer <timothyofficer@gmail.com>
2016-03-09 09:59:14 -0500

    use correct domain in dev and prod environments, cookies aren’t working because of it

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,11 +4,11 @@ const _ = require('underscore'); //jshint ignore:line
 
 const NODE_ENV = process.env.WERCKER_GIT_BRANCH || process.env.NODE_ENV || process.argv[3];
 const ENV = _setupEnv(NODE_ENV);
+const PORT = process.env.PORT || 2020;
 
 const defaultSettings = {
-  PORT: process.env.PORT || 2020,
+  PORT: PORT,
   BUILD_DIR: 'build',
-  DOMAIN: 'reviewstartups.com',
   ENABLE_COOKIE_DOMAIN: false,
   ENABLE_SECURE_COOKIE: false,
   MAILCHIMP_USERNAME: 'thestartupreview',
@@ -22,24 +22,28 @@ const production = _.extend(_.clone(defaultSettings), {
   API: 'https://api.startupreview.com',
   ENABLE_COOKIE_DOMAIN: true,
   ENABLE_SECURE_COOKIE: true,
-  GOOGLE_ANALYTICS: 'UA-74655471-1'
+  GOOGLE_ANALYTICS: 'UA-74655471-1',
+  DOMAIN: 'http://www.reviewstartups.com'
 });
 
 const development = _.extend(_.clone(defaultSettings), {
   ENV: 'development',
   API: 'https://api-dev.startupreview.com',
   ENABLE_COOKIE_DOMAIN: true,
-  ENABLE_SECURE_COOKIE: true
+  ENABLE_SECURE_COOKIE: true,
+  DOMAIN: 'http://www-dev.reviewstartups.com'
 });
 
 const local = _.extend(_.clone(defaultSettings), {
   ENV: 'local',
-  API: 'http://localhost:3001'
+  API: 'http://localhost:3001',
+  DOMAIN: `http://localhost:${PORT}`
 });
 
 const test = _.extend(_.clone(defaultSettings), { //jshint ignore:line
   ENV: 'test',
-  API: 'http://localhost:3001'
+  API: 'http://localhost:3001',
+  DOMAIN: `http://localhost:${PORT}`
 });
 
 const ENVS = {
