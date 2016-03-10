86412328b27ff9cd1c26b83a4e6d40d045029c27
Tim Officer <timothyofficer@gmail.com>
2016-03-10 13:05:48 -0500

    require new relic in production ENV

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const appConfig = require('./config/appConfig');
 const app = require('./config/expressConfig').configure();
 
+if (appConfig.ENV === 'production') {
+  require('newrelic');
+}
+
 app.listen(appConfig.PORT, () => {
   console.log('Listening on port: ' + appConfig.PORT);
 });
